784dd8149e0d8747de77cd43305f958c3c98cc27
Yunmonkey <yts0646@naver.com>
2019-01-16 22:33:11 +0900

    WithFuction Update

--- SecurityApplication/app/src/main/java/com/tongmyung/yun/securityapplication/HashMapadd.kt
+++ SecurityApplication/app/src/main/java/com/tongmyung/yun/securityapplication/HashMapadd.kt
@@ -2,8 +2,8 @@ package com.tongmyung.yun.securityapplication
 
 class HashMapadd{
 
-    var S0_map = HashMap<Int, ArrayList<Int>>()
-    var S1_map = HashMap<Int, ArrayList<Int>>()
+    var S0_map = HashMap<Int, List<Int>>()
+    var S1_map = HashMap<Int, List<Int>>()
     val first_list_S0 = arrayOf(1,0,3,2)
     val second_list_S0 = arrayOf(3,2,1,0)
     val third_list_S0 = arrayOf(0,2,1,3)
@@ -23,6 +23,8 @@ class HashMapadd{
         var second_S0 = ArrayList<Int>()
         var third_S0 = ArrayList<Int>()
         var firth_S0 = ArrayList<Int>()
+
+        val s0_mapInput = listOf(first_S0, second_S0, third_S0, firth_S0)
 
         fun first_S0_init() {
             for(i in 0 until 4) {
@@ -48,11 +50,11 @@ class HashMapadd{
             }
         }
 
-        fun S0_map_init () {
-            S0_map.put(0,first_S0)
-            S0_map.put(1,second_S0)
-            S0_map.put(2, third_S0)
-            S0_map.put(3, firth_S0)
+        //with 라는 람다수식지정함수 를 사용하여 S0_map 을 지정해주고 객체 사용 빈도수를 확 줄임
+        fun S0_map_init () = with(S0_map) {
+            for(index in 0 until 4) {
+                this[index] = s0_mapInput[index]
+            }
         }
 
         first_S0_init()
@@ -66,6 +68,8 @@ class HashMapadd{
         var second_S1 = ArrayList<Int>()
         var third_S1 = ArrayList<Int>()
         var firth_S1 = ArrayList<Int>()
+
+        val s1_mapInput = listOf(first_S1, second_S1, third_S1, firth_S1)
 
         fun first_S1_init() {
             for(i in 0 until 4) {
@@ -91,11 +95,11 @@ class HashMapadd{
             }
         }
 
-        fun S1_map_init() {
-            S1_map.put(0,first_S1)
-            S1_map.put(1,second_S1)
-            S1_map.put(2, third_S1)
-            S1_map.put(3, firth_S1)
+        //with 라는 람다수식지정함수 를 사용하여 S1_map 을 지정해주고 객체 사용 빈도수를 확 줄임
+        fun S1_map_init() = with(S1_map) {
+            for(index in 0 until 4) { //0..3
+                this[index] = s1_mapInput[index]
+            }
         }
 
         first_S1_init()
